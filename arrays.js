ea89ef4c17f727b6e0a25f4248435e9ecc5a13fe
Alyona <itsbronza@yahoo.com>
2019-11-03 15:10:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element){
-var array = ['one', 'two', 'three'];
+var array = ['foo', 1];
 array.unshift('element')
 }
 
